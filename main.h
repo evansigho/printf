a61aa581a5e1e7260f5ad98e5dfdfed264af3ffc
jerryigho <ighojerry@gmail.com>
2022-11-22 14:23:04 -0800

    this is a collaborative printf projec

--- main.h
+++ main.h
@@ -68,7 +68,7 @@ int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
-		char buffer[], int flags, char flag_ch, int width, int precision, int size);
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
@@ -103,7 +103,7 @@ int write_pointer(char buffer[], int ind, int length,
 		int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
-		char buffer[],
+char buffer[],
 		int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
