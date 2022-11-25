fea6b0fe086dacce2ee49d4f167a4dcbb4e45038
evansigho <evansigho4@gmail.com>
2022-11-25 09:02:17 -0800

    new edit on printf

--- get_precision.c
+++ get_precision.c
@@ -11,7 +11,7 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	nt precision = -1;
+	int precision = -1;
 
 	if (format[curr_i] != '.')
 		return (precision);
--- main.h
+++ main.h
@@ -48,7 +48,7 @@ int handle_print(const char *fmt, int *i,
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[]
+int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
